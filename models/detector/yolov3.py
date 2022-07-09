37af76ead3c876a2ce910896b437e4307ff93977
myungsanglee <lee0577@naver.com>
2022-07-09 16:14:55 +0900

    update

--- models/detector/yolov3.py
+++ models/detector/yolov3.py
@@ -3,9 +3,8 @@ import sys
 sys.path.append(os.getcwd())
 
 import torch
-from torch import dropout, nn
+from torch import nn
 import torchsummary
-import timm
 
 from models.initialize import weight_initialize
 from models.layers.conv_block import Conv2dBnRelu
@@ -20,30 +19,38 @@ class YoloV3(nn.Module):
         self.num_classes = num_classes
         self.num_anchors = int(num_anchors/3)
 
-        self.p4_route = nn.Sequential(
+        self.c3_conv = nn.Sequential(
+            Conv2dBnRelu(512, 256, 3)
+        )
+
+        self.c4_conv = nn.Sequential(
+            Conv2dBnRelu(1024, 512, 3)
+        )
+
+        self.c4_route = nn.Sequential(
             Conv2dBnRelu(512, 256, 3),
             
             nn.UpsamplingNearest2d(scale_factor=2)
         )
 
-        self.p5_route = nn.Sequential(
+        self.c5_route = nn.Sequential(
             Conv2dBnRelu(1024, 512, 3),
 
             nn.UpsamplingNearest2d(scale_factor=2)
         )
 
         self.p3_head = nn.Sequential(
+            Conv2dBnRelu(256, 128, 3),
+            Conv2dBnRelu(128, 256, 3),
+            
+            nn.Conv2d(256, (self.num_anchors*(self.num_classes + 5)), 1, 1, bias=False)
+        )
+
+        self.p4_head = nn.Sequential(
             Conv2dBnRelu(512, 256, 3),
             Conv2dBnRelu(256, 512, 3),
             
             nn.Conv2d(512, (self.num_anchors*(self.num_classes + 5)), 1, 1, bias=False)
-        )
-
-        self.p4_head = nn.Sequential(
-            Conv2dBnRelu(1024, 512, 3),
-            Conv2dBnRelu(512, 1024, 3),
-            
-            nn.Conv2d(1024, (self.num_anchors*(self.num_classes + 5)), 1, 1, bias=False)
         )
         
         self.p5_head = nn.Sequential(
@@ -52,8 +59,6 @@ class YoloV3(nn.Module):
             
             nn.Conv2d(1024, (self.num_anchors*(self.num_classes + 5)), 1, 1, bias=False)
         )
-
-        # self.dropout = nn.Dropout2d(p=0.5)
 
 
     def forward(self, x):
@@ -66,20 +71,19 @@ class YoloV3(nn.Module):
         c5 = self.backbone.layer5(c4) # [batch_size, 1024, input_size/32, input_size/32]
 
         # Prediction Branch
-        # c5 = self.dropout(c5)
         p5 = self.p5_head(c5)
 
         # Prediction Branch
-        p5_route = self.p5_route(c5)
-        p4 = torch.cat((p5_route, c4), 1)
-        # p4 = self.dropout(p4)
-        p4 = self.p4_head(p4)
+        c5_route = self.c5_route(c5)
+        c4 = torch.cat((c5_route, c4), 1) # [batch_size, 1024, input_size/16, input_size/16]
+        c4 = self.c4_conv(c4)
+        p4 = self.p4_head(c4)
 
         # Prediction Branch
-        p4_route = self.p4_route(c4)
-        p3 = torch.cat((p4_route, c3), 1)
-        # p3 = self.dropout(p3)
-        p3 = self.p3_head(p3)
+        c4_route = self.c4_route(c4)
+        c3 = torch.cat((c4_route, c3), 1) # [batch_size, 512, input_size/8, input_size/8]
+        c3 = self.c3_conv(c3)
+        p3 = self.p3_head(c3)
 
         return p3, p4, p5
 
@@ -92,7 +96,7 @@ if __name__ == '__main__':
     model = YoloV3(
         backbone=backbone,
         num_classes=20,
-        num_anchors=3
+        num_anchors=9
     )
 
     tmp_input = torch.randn((1, 3, input_size, input_size))
@@ -105,12 +109,12 @@ if __name__ == '__main__':
 
     torchsummary.summary(model, (3, input_size, input_size), batch_size=1, device='cpu')
 
-    # from module.yolov2_detector import YoloV2Detector
+    # from module.yolov3_detector import YoloV3Detector
     # from utils.yaml_helper import get_configs
 
-    # model = YoloV2Detector(
+    # model = YoloV3Detector(
     #     model=model,
-    #     cfg=get_configs('configs/yolov2_voc.yaml')
+    #     cfg=get_configs('configs/yolov3_voc.yaml')
     # )
     # file_path = 'model.onnx'
     # input_sample = torch.randn((1, 3, 416, 416))
