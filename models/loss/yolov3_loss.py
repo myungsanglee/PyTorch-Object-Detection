348392941e9290625097d82f4e5275b270891d19
myungsanglee <lee0577@naver.com>
2023-01-18 14:30:06 +0900

    update

--- models/loss/yolov3_loss.py
+++ models/loss/yolov3_loss.py
@@ -360,3 +360,181 @@ class YoloV3LossV2(nn.Module):
                 tcls[b, best_n, gj, gi, int(target[b, t, 4])] = self.positive_class_target
                 
         return mask, noobj_mask, tbox, tconf, tcls
+
+
+class YoloV3LossV3(nn.Module):
+    """YoloV3 Loss Function
+
+    Arguments:
+      num_classes (int): Number of classes in the dataset
+      anchors (List): Anchors of a specific dataset, [num_anchors, 2(anchor_w, anchor_h)]
+      input_size (int): input size of Model (image input size)
+    """
+    
+    def __init__(self, num_classes, anchors, input_size):
+        super().__init__()
+        self.num_classes = num_classes
+        self.anchors = anchors
+        self.input_size = input_size
+
+        # These are from Yolo paper, signifying how much we should
+        # pay loss for no object (noobj) and the box coordinates (coord)
+        self.lambda_obj = 5.
+        self.lambda_box = 1.
+        self.lambda_cls = 1.
+        
+        self.balance = [4.0, 1.0, 0.4]
+        
+        self.ignore_threshold = 0.5
+
+        # self.bce_cls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([1.]))
+        # self.bce_obj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([1.]))
+        
+        self.bce_cls = nn.BCELoss(reduction='sum')
+        self.bce_obj = nn.BCELoss(reduction='sum')
+        # self.mse_obj = nn.MSELoss(reduction='sum')
+        
+        self.positive_class_target, self.negative_class_target = smooth_BCE(0.)
+
+    def forward(self, input, target):
+        """
+        Arguments:
+            input (list): list of yolov3 output layers, [p3, p4, p5] each layers size is [batch, 3*(5+num_classes), layer_h, layer_w]
+            target (tensor): [batch, max_num_annots, 5(cx, cy, w, h, cid)]
+            
+        Returns:
+            loss (float): total loss values
+        """
+        device = input[0].device
+        lcls = torch.zeros(1, device=device)  # class loss
+        lbox = torch.zeros(1, device=device)  # box loss
+        lobj = torch.zeros(1, device=device)  # object loss
+        lnobj = torch.zeros(1, device=device)  # no object loss
+        batch_size = input[0].size(0)
+        for layer_idx, pred in enumerate(input):
+            _, _, layer_h, layer_w = pred.size()
+            
+            # [batch_size, num_anchors, 5+num_classes, layer_h, layer_w] to [batch_size, num_anchors, layer_h, layer_w, 5+num_classes]
+            prediction = pred.view(batch_size, 3, -1, layer_h, layer_w).permute(0, 1, 3, 4, 2).contiguous()
+            
+            pxy = torch.sigmoid(prediction[..., 0:2])
+            pwh = torch.exp(prediction[..., 2:4])
+            pbox = torch.cat([pxy, pwh], dim=-1)
+            pconf = torch.sigmoid(prediction[..., 4])
+            pcls = torch.sigmoid(prediction[..., 5:])         
+            
+            mask, num_targets, tbox, tconf, tcls = self._encode_target(target, self.num_classes, layer_idx, self.anchors, layer_w, layer_h, self.ignore_threshold)
+            if prediction.is_cuda:
+                mask = mask.cuda()
+                tbox = tbox.cuda()
+                tconf = tconf.cuda()
+                tcls = tcls.cuda()
+            
+            if num_targets:
+                # ============================ #
+                #   FOR BOX COORDINATES Loss   #
+                # ============================ #
+                box_iou = bbox_iou(pbox[mask==1], tbox[mask==1], CIoU=True).squeeze()
+                lbox += (1.0 - box_iou).sum()
+                # print(f'lbox: {lbox}')
+                # print(f'box_iou: {box_iou.size()}')
+            
+                # ==================== #
+                #   FOR OBJECT LOSS    #
+                # ==================== #
+                # iou = box_iou.detach().clamp(0).type(tconf.dtype)
+                # tconf[mask==1] = iou
+                # print(f'tconf: {tconf[mask==1].size()}')
+                lobj += self.bce_obj(pconf[tconf==1], tconf[tconf==1])
+            
+                # ================== #
+                #   FOR CLASS LOSS   #
+                # ================== #
+                lcls += self.bce_cls(pcls[mask==1], tcls[mask==1])
+                # print(f'lcls: {lcls}\n')
+            
+            # obji = self.bce_obj(pconf, tconf)
+            # obji = self.mse_obj(pconf, tconf)
+            # lobj += obji * self.balance[layer_idx]
+            lnobj += self.bce_obj(pconf[(1-tconf)==1], tconf[(1-tconf)==1])
+            
+        lbox *= self.lambda_box
+        lobj *= self.lambda_obj
+        lcls *= self.lambda_cls
+        # loss = (lbox + lobj + lcls) / batch_size
+        loss = (lbox + lobj + lcls + lnobj) / batch_size
+        
+        return loss
+
+    def _encode_target(self, target, num_classes, layer_idx, anchors, layer_w, layer_h, ignore_threshold):
+        """YoloV3 Loss Function
+
+        Arguments:
+            target (Tensor): [batch, max_num_annots, 5(cx, cy, w, h, cid)]
+            num_classes (int): Number of classes in the dataset
+            layer_idx (int): index of yolov3 ouput layers
+            anchors (List): Anchors of a specific dataset, [total_num_anchors, 2(scaled_w, scaled_h)]
+            layer_w (int): size of predictions width
+            layer_h (int): size of predictions height
+            ignore_threshold (float): float value of ignore iou
+        
+        Retruns:
+            mask (Tensor): Objectness Mask Tensor, [batch_size, num_anchors, layer_h, layer_w]
+            num_targets (int): Number of targets
+            tbox (Tensor): Ground Truth Box, [batch_size, num_anchors, layer_h, layer_w, 4]
+            tconf (Tensor): Ground Truth Confidence Score, [batch_size, num_anchors, layer_h, layer_w]
+            tcls (Tensor): Ground Truth Class index, [batch_size, num_anchors, layer_h, layer_w, num_classes]
+        """
+        batch_size = target.size(0)
+        num_anchors = 3
+        
+        scaled_anchors = [[anchor_w * (layer_w/self.input_size), anchor_h * (layer_h/self.input_size)] for anchor_w, anchor_h in anchors[3*layer_idx:3*layer_idx+3]]
+        
+        mask = torch.zeros(batch_size, num_anchors, layer_h, layer_w)
+        tbox = torch.zeros(batch_size, num_anchors, layer_h, layer_w, 4)
+        tconf = torch.zeros(batch_size, num_anchors, layer_h, layer_w)
+        tcls = torch.zeros(batch_size, num_anchors, layer_h, layer_w, num_classes)
+
+        for b in torch.arange(batch_size):
+            for t in torch.arange(target.size(1)):
+                if target[b, t].sum() <= 0:
+                    continue
+                
+                # check this target is correct for this layer_idx
+                gw = target[b, t, 2] * self.input_size
+                gh = target[b, t, 3] * self.input_size
+                
+                gt_box = torch.FloatTensor([0, 0, gw, gh]).unsqueeze(0) # [1, 4]
+                anchors_box = torch.cat([torch.zeros((len(anchors), 2), dtype=torch.float32), torch.FloatTensor(anchors)], 1) # [total_num_anchors, 4]
+                
+                calc_iou = bbox_iou(gt_box, anchors_box, x1y1x2y2=True) # [total_num_anchors, 1]
+                calc_iou = calc_iou.squeeze(dim=-1) # [total_num_anchors]
+                
+                best_n = torch.argmax(calc_iou)
+                
+                if layer_idx*3 > best_n or best_n >= layer_idx*3+3:
+                    continue
+                else:
+                    calc_iou = calc_iou[layer_idx*3:layer_idx*3+3]
+                    best_n -= layer_idx*3 
+                
+                gx = target[b, t, 0] * layer_w
+                gy = target[b, t, 1] * layer_h
+                gw = target[b, t, 2] * layer_w
+                gh = target[b, t, 3] * layer_h
+                gi = int(gx)
+                gj = int(gy)
+                
+                mask[b, best_n, gj, gi] = 1
+                tbox[b, best_n, gj, gi] = torch.tensor([gx - gi, gy - gj, gw/scaled_anchors[best_n][0], gh/scaled_anchors[best_n][1]])
+                tconf[b, best_n, gj, gi] = 1
+                tconf[b, calc_iou > ignore_threshold, gj, gi] = 1
+                # tcls[b, best_n, gj, gi, int(target[b, t, 4])] = 1
+                
+                # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf
+                tcls[b, best_n, gj, gi, :] = self.negative_class_target
+                tcls[b, best_n, gj, gi, int(target[b, t, 4])] = self.positive_class_target
+        
+        num_targets = torch.where(mask==1)[0].size(0)
+        
+        return mask, num_targets, tbox, tconf, tcls
--- module/yolov3_detector.py
+++ module/yolov3_detector.py
@@ -1,7 +1,9 @@
 import pytorch_lightning as pl
+from torch import nn
+from torch.optim import SGD
 
 from utils.module_select import get_optimizer, get_scheduler
-from models.loss.yolov3_loss import YoloV3Loss, YoloV3LossV2
+from models.loss.yolov3_loss import YoloV3Loss, YoloV3LossV2, YoloV3LossV3
 from utils.yolov3_utils import MeanAveragePrecision
 
 
@@ -12,6 +14,7 @@ class YoloV3Detector(pl.LightningModule):
         self.model = model
         # self.loss_fn = YoloV3Loss(cfg['num_classes'], cfg['anchors'], cfg['input_size'])
         self.loss_fn = YoloV3LossV2(cfg['num_classes'], cfg['anchors'], cfg['input_size'])
+        # self.loss_fn = YoloV3LossV3(cfg['num_classes'], cfg['anchors'], cfg['input_size'])
         self.map_metric = MeanAveragePrecision(cfg['num_classes'], cfg['anchors'], cfg['input_size'], cfg['conf_threshold'])
 
     def forward(self, x):
@@ -45,11 +48,26 @@ class YoloV3Detector(pl.LightningModule):
 
     def configure_optimizers(self):
         cfg = self.hparams.cfg
-        optim = get_optimizer(
-            cfg['optimizer'],
-            self.model.parameters(),
-            **cfg['optimizer_options']
-        )
+        
+        g0, g1, g2 = [], [], []  # optimizer parameter groups
+        for v in self.model.modules():
+            if hasattr(v, 'bias') and isinstance(v.bias, nn.Parameter):  # bias
+                g2.append(v.bias)
+            if isinstance(v, nn.BatchNorm2d):  # weight (no decay)
+                g0.append(v.weight)
+            elif hasattr(v, 'weight') and isinstance(v.weight, nn.Parameter):  # weight (with decay)
+                g1.append(v.weight)
+        
+        optim = SGD(g0, lr=cfg['optimizer_options']['lr'], momentum=cfg['optimizer_options']['momentum'], nesterov=True)
+        
+        optim.add_param_group({'params': g1, 'weight_decay': cfg['optimizer_options']['weight_decay']})  # add g1 with weight_decay
+        optim.add_param_group({'params': g2})  # add g2 (biases)
+        
+        # optim = get_optimizer(
+        #     cfg['optimizer'],
+        #     self.model.parameters(),
+        #     **cfg['optimizer_options']
+        # )
         
         try:
             scheduler = get_scheduler(
